92436222b1ff269a9a8b33b47542afc84e511518
do-huni <orangnlp@gmail.com>
2025-02-16 17:33:47 +0900

    fix: boolean parser

--- src/shared/utils/createAstroContent.ts
+++ src/shared/utils/createAstroContent.ts
@@ -4,11 +4,16 @@ export function createAstroContent<T extends AstroContent>(
   astroContent: T,
 ): string {
   // frontmatter 생성
+  console.log(astroContent.frontmatter);
   const frontmatterEntries = Object.entries(astroContent.frontmatter)
     .map(([key, value]) => {
       // 배열인 경우 YAML 형식으로 변환
       if (Array.isArray(value)) {
         return `${key}:\n${value.map((item) => `  - ${item}`).join('\n')}`;
+      }
+      // boolean인 경우 true, false로 변환
+      if (typeof value === 'boolean') {
+        return `${key}: ${value ? 'true' : 'false'}`;
       }
       // 문자열인 경우 따옴표로 감싸기
       return `${key}: "${value}"`;
